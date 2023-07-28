55839f44feecbb86d0b05ac2a4e033ae33871785
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-07-28 15:18:49 +0600

    done with cards

--- client/src/component/rooms/Rooms.jsx
+++ client/src/component/rooms/Rooms.jsx
@@ -9,7 +9,11 @@ const Rooms = () => {
     },[])
     return (
         <Container>
-            {rooms.length}
+        <div className=' pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+{
+    rooms.map(room => <p>{room.location}</p>)
+}
+        </div>
         </Container>
     );
 };
