db54f9ecc27d3b4f43e20bc15de8f6de4f6c6e2a
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-07-28 16:24:21 +0600

    done with home layout

--- client/src/component/rooms/Rooms.jsx
+++ client/src/component/rooms/Rooms.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import Container from '../shared/Container';
+import React, { useEffect, useState } from "react";
+import Container from "../shared/Container";
+import Card from "../card/Card";
+import Loader from "../shared/Loader";
 
 const Rooms = () => {
-    const [rooms, setRooms] = useState([])
-    useEffect(()=>{
-        fetch("rooms.json").then(res => res.json()).then(data => setRooms(data))
-        .catch(err => console.log(err))
-    },[])
-    return (
-        <Container>
-        <div className=' pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-{
-    rooms.map(room => <p>{room.location}</p>)
-}
-        </div>
-        </Container>
-    );
+  const [rooms, setRooms] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch("rooms.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setRooms(data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+
+  if(loading){
+    return <Loader></Loader>
+  }
+  return (
+    <Container>
+      <div className=" pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+        {rooms.map((room, index) => (
+          <Card key={index} room ={room}></Card>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Rooms;
